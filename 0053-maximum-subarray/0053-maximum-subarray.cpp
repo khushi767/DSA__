22e05476ac776b17d4a96a666ce5fa2dad0d8925
khushi kumari <99918465+khushi767@users.noreply.github.com>
2024-09-13 22:29:17 +0530

    Time: 78 ms (50.51%), Space: 70.7 MB (22.72%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -19,15 +19,17 @@ public:
     //---------------------------------------------------------
 //     int sum=0;
 // int maxi=INT_MIN;
-//     for(int i=0;i<nums.size();i++){
-//         for(int j=i;i<nums.size();i++){
-//             sum+=nums[i];
-//             maxi=max(maxi,sum);
-//         }
+//    for (int i = 0; i < nums.size(); i++) {
+//     sum = 0; // Reset sum for each subarray starting from index i
+//     for (int j = i; j < nums.size(); j++) { // Use j instead of i for the inner loop
+//         sum += nums[j]; // Sum subarray elements from i to j
+//         maxi = max(maxi, sum); // Update maxi if sum is larger
 //     }
+//    }
 //     return maxi;
-//     }                   O(n^2)
+//     }                  // O(n^2)
 //--------------------------------------------------------------
+//KADANE ALGORITHM
 long long maxi=LONG_MIN;
 long long sum=0;
 for(int i=0;i<nums.size();i++){
