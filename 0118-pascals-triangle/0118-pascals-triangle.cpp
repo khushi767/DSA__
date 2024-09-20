843bd74d1a3fe1fc5f7a5c49b829eba94603e417
khushi kumari <99918465+khushi767@users.noreply.github.com>
2024-09-21 02:18:15 +0530

    Time: 0 ms (100%), Space: 8.3 MB (42.18%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,25 +1,35 @@
-class Solution {
-public:
-    vector<vector<int>> generate(int numRows) {
-      if(numRows==0)return   {};
-      if(numRows==1)return {{1}};
-      vector<vector<int>>prevRows=generate(numRows-1);
-      vector<int>newRow(numRows,1);
-      for(int i=1;i<numRows-1;i++){
-        newRow[i]=prevRows.back()[i-1]+prevRows.back()[i];
-      }
-      prevRows.push_back(newRow);
-      return prevRows;
-    }
-};
+// class Solution {
+// public:
+//     vector<vector<int>> generate(int numRows) {
+//       if(numRows==0)return   {};
+//       if(numRows==1)return {{1}};
+//       vector<vector<int>>prevRows=generate(numRows-1);
+//       vector<int>newRow(numRows,1);
+//       for(int i=1;i<numRows-1;i++){
+//         newRow[i]=prevRows.back()[i-1]+prevRows.back()[i];
+//       }
+//       prevRows.push_back(newRow);
+//       return prevRows;
+//     }
+// };
 
 //------------------------------
 // class Solution {
 // public:
 //     vector<vector<int>> generate(int numRows) {
-        
+//       vector<vector<int>>result;
+//       for(int i=0;i<numRows;i++){
+//         vector<int>row(i+1,1);
+//         for(int j=1;j<i;j++){
+//             row[j]=result[i-1][j-1]+result[i-1][j];
+//         }
+//         result.push_back(row);
+//       }  
+//       return result;
 //     }
 // };
+
+
 //--------------------------------
 // class Solution {
 // public:
@@ -27,3 +37,23 @@ public:
         
 //     }
 // };
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        vector<int> prevRow;
+        
+        for (int i = 0; i < numRows; i++) {
+            vector<int> currentRow(i + 1, 1);
+            
+            for (int j = 1; j < i; j++) {
+                currentRow[j] = prevRow[j - 1] + prevRow[j];
+            }
+            
+            result.push_back(currentRow);
+            prevRow = currentRow;
+        }
+        
+        return result;
+    }
+};
