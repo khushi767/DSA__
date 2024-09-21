0a6a6834806fb0c2690a84b65e82a0d7750330c7
khushi kumari <99918465+khushi767@users.noreply.github.com>
2024-09-22 04:11:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+public:
+    int findKRotation(vector<int> &arr) {
+        int n=arr.size();
+        int ans=INT_MAX, index=-1;
+        for(int i=0;i<n;i++){
+            if(arr[i]<ans){
+                ans=arr[i];
+                index=i;
+            }
+        }
+        return index;
+    }
+    };
+    
+  
+    //------------------------------------------------------
+// class Solution {
+// public:
+//     int findKRotation(vector<int> &arr) {
+//         int low = 0, high = arr.size() - 1;
+//         int ans = INT_MAX;  // To track the minimum value
+//         int index = -1;     // To track the index of the minimum element
+
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
+
+//             // Case 1: If the array/subarray is sorted, arr[low] is the smallest element.
+//             if (arr[low] <= arr[high]) {
+//                 if (arr[low] <= ans) {
+//                     ans = arr[low];
+//                     index = low;  // Track the index of the minimum element
+//                 }
+//                 break;  // No need to search further
+//             }
+
+//             // Case 2: If the left half is sorted
+//             if (arr[low] <= arr[mid]) {
+//                 if (arr[low] <= ans) {
+//                     ans = arr[low];
+//                     index = low;  // Update index with low if it's the smallest
+//                 }
+//                 low = mid + 1;  // Move to the right half
+//             }
+//             // Case 3: If the right half is sorted
+//             else {
+//                 if (arr[mid] < ans) {
+//                     ans = arr[mid];
+//                     index = mid;  // Update index with mid if it's the smallest
+//                 }
+//                 high = mid - 1;  // Move to the left half
+//             }
+//         }
+
+//         return index;  // Return the index of the minimum element (rotation count)
+//     }
+// };
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input1;
+        getline(cin, input1);
+        stringstream ss1(input1);
+        int number1;
+        while (ss1 >> number1) {
+            arr.push_back(number1);
+        }
+        Solution ob;
+        int res = ob.findKRotation(arr);
+        cout << res << endl;
+    }
+}
+// } Driver Code Ends
