065fdf18d619ad9874662d952cb54078eec28600
khushi kumari <99918465+khushi767@users.noreply.github.com>
2024-10-15 23:49:39 +0530

    Time: 5 ms (47.54%), Space: 8.1 MB (89.31%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -6,17 +6,19 @@ public:
         for(int i=0;i<s.size();i++){
             if(s[i]=='('){
                 if(depth>0){
-                    result+=s[i];//append only if we are not at outermost level
-                }
-                depth++;
-            }else{
-                depth--;//decrease depth means we encounter ')'
-                if(depth>0){
                     result+=s[i];
                 }
+                depth++;
             }
+                else{
+                    depth--;
+                    if(depth>0){
+                        result+=s[i];
+                    }
+                }
+            }
+            return result;
         }
-        return result;
-    }
 };//TC=O(n), SC=O(n) n is lenght of string s
+
  
