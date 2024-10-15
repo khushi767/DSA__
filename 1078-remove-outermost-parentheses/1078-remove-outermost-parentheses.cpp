fa220cb8e9b7fac136587f30dd6ffcd4a9b2d6ef
khushi kumari <99918465+khushi767@users.noreply.github.com>
2024-10-15 23:32:34 +0530

    Time: 0 ms (100%), Space: 8.1 MB (73.52%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -3,20 +3,20 @@ public:
     string removeOuterParentheses(string s) {
         string result;
         int depth=0;
-        for(char c:s){
-            if(c=='('){
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
                 if(depth>0){
-                    result+=c;//append only if we are not at outermost level
+                    result+=s[i];//append only if we are not at outermost level
                 }
                 depth++;
             }else{
                 depth--;//decrease depth means we encounter ')'
                 if(depth>0){
-                    result+=c;
+                    result+=s[i];
                 }
             }
         }
         return result;
     }
-};
+};//TC=O(n), SC=O(n) n is lenght of string s
  
