7eb3019f144dcc33cb1d7c9d82f6a90d7601c4d2
khushi kumari <99918465+khushi767@users.noreply.github.com>
2024-10-26 19:38:20 +0530

    Time: 95 ms (40.91%), Space: 54 MB (7.09%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -15,21 +15,39 @@
 //     }
 // };              TC=O(n^3)  SC=O(1)
 //------------------------------
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+// int n=nums.size();
+// int cnt=0;
+// for(int i=0;i<n;i++){
+//     int sum=0;
+//     for(int j=i;j<n;j++){
+//         sum+=nums[j];
+//         if(sum==k)
+//         cnt++;
+//     }
+// }
+// return cnt;
+//     }
+// };         TC=O(n^2) SC=O(1)
+
+
+//----------------------------------
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
 int n=nums.size();
-int cnt=0;
+map<int,int>mpp;
+int presum=0,cnt=0;
+mpp[0]=1;
 for(int i=0;i<n;i++){
-    int sum=0;
-    for(int j=i;j<n;j++){
-        sum+=nums[j];
-        if(sum==k)
-        cnt++;
-    }
+    presum+=nums[i];
+    int remove=presum-k;
+    cnt+=mpp[remove];
+    mpp[presum]+=1;
 }
 return cnt;
     }
 };
-
 
