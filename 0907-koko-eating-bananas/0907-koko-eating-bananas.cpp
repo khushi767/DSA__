9b4ef38fef4f57828af907c10a5ca9596a4c7e3b
khushi kumari <99918465+khushi767@users.noreply.github.com>
2024-10-30 01:24:30 +0530

    Time: 11 ms (45.41%), Space: 21.7 MB (43.05%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,3 +1,5 @@
+
+//Brute force: O(max(a[]) * N), sc=O(1)
 // #include<bits/stdc++.h>
 // class Solution {
 // public:
@@ -9,6 +11,9 @@
 //         int n=piles.size();
 //         for(int i=0;i<n;i++){
 //             totalh+=ceil((double)(piles[i])/(double)(hourly));
+//             if(totalh>INT_MAX){
+//                 return INT_MAX;
+//             }
 //         }
 //         return totalh;
 //     }
@@ -24,38 +29,35 @@
 //     }
 // };
 
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution {
-public:
-    int findmax(vector<int>& piles) {
-        return *max_element(piles.begin(), piles.end());
+//---------------------------------BETTER APPROACH USING BINARY TREE
+#include<bits/stdc++.h>
+class Solution{
+    public:
+    int findmax(vector<int>&piles){
+        return *max_element(piles.begin(),piles.end());
     }
-
-    int calculatetotalhours(vector<int>& piles, int hourly) {
-        long long totalh = 0;  // Use long long to avoid overflow
-        int n = piles.size();
-        for (int i = 0; i < n; i++) {
-            totalh += ceil((double)(piles[i]) / (double)(hourly));
-            if (totalh > INT_MAX) { // Optional check to save computation time if already exceeding limit
-                return INT_MAX;
-            }
+    int calculatetotalhours(vector<int>&piles,int hourly){
+        long long totalh=0;
+        int n=piles.size();
+        for(int i=0;i<n;i++){
+totalh+=ceil((double)(piles[i])/(double)(hourly));
+if(totalh>INT_MAX){
+    return INT_MAX;
+}
         }
         return totalh;
     }
-
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1, right = findmax(piles);
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            int reqtime = calculatetotalhours(piles, mid);
-            if (reqtime <= h) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
+int minEatingSpeed(vector<int>&piles,int h){
+    int left=1,right=findmax(piles);
+    while(left<right){
+        int mid=left+(right-left)/2;
+        int reqtime=calculatetotalhours(piles,mid);
+        if(reqtime<=h){
+            right=mid;
+        }else{
+            left=mid+1;
         }
-        return left;
     }
+    return left;
+}
 };
