bffab05a775800b4bfb888b63562cdc06fa1cdb4
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-09-27 03:37:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.cpp
+++ Difficulty: Easy/Palindrome String/palindrome-string.cpp
@@ -2,8 +2,18 @@ class Solution {
   public:
     bool isPalindrome(string& s) {
         // code here
-        string pal=s;
+       /* string pal=s;
         reverse(s.begin(),s.end());
+        return s==pal;
+        */
+        string pal=s;
+        int left=0;
+        int right=s.size()-1;
+        while(left<right){
+            swap(s[left],s[right]);
+            left++;
+            right--;
+        }
         return s==pal;
     }
     
