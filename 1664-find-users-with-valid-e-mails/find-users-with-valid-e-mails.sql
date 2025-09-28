da294ddf76e808ac1d38d643902f9ffe1d153c7d
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-09-28 09:07:31 +0530

    Time: 813 ms (24.75%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -2,4 +2,15 @@
 select * from users where
 REGEXP_like(mail,'^[A-Za-z][A-Za-z0-9._-]*@leetcode\\.com$','c')
 
+/*
+^ → Start of the string
 
+[A-Za-z] → First character must be a letter
+
+[A-Za-z0-9._-]* → Any number of allowed characters
+
+@leetcode\.com → Domain must be exactly @leetcode.com
+
+dollar sign → End of the string
+
+'c' → Case-sensitive match*/
