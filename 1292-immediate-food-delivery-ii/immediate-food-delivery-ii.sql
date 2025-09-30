12fc740908e29eedfe41131362351e3bb4d826c4
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-09-30 20:04:54 +0530

    Time: 743 ms (24.91%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,15 +1,4 @@
-# Write your MySQL query statement below
-
-SELECT 
-  ROUND(
-    100 * SUM(order_date = customer_pref_delivery_date) / COUNT(*), 
-    2
-  ) AS immediate_percentage
-FROM Delivery d
-JOIN (
-    SELECT customer_id, MIN(order_date) AS first_order_date
-    FROM Delivery
-    GROUP BY customer_id
-) f 
-ON d.customer_id = f.customer_id 
-AND d.order_date = f.first_order_date;
+select round(sum(if(order_date=customer_pref_delivery_date,1,0))*100/count(distinct customer_id),2)as immediate_percentage from Delivery
+where(customer_id,order_date)in
+(select customer_id,min(order_date)as first_order_date
+from Delivery group by customer_id)
