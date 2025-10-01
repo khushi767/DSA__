5d111b3c15f4d259673e13e604a10de66cca8b4f
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-10-02 03:01:04 +0530

    Time: 361 ms (97.44%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -3,3 +3,7 @@ from MyNumbers
 group by num
 having count(num) = 1
 )as unique_numbers;
+
+/*
+SELECT IF(COUNT(num) =1, num, null) AS num FROM MyNumbers GROUP BY num ORDER BY COUNT(num), num DESC LIMIT 1;
+*/
