5bcd8106fb6f906bee821f4e0a7c21f2238aab03
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-10-10 04:03:15 +0530

    Time: 3 ms (21.24%) | Memory: 23.1 MB (62.21%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -1,6 +1,9 @@
+/*
+USING HASHMAP
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
+     
         int freq[101]={0};
         for(int i:nums){
             freq[i]++;
@@ -15,5 +18,27 @@ public:
     }
 };
 
+*/
+//USING MAPS
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+    unordered_map<int,int>freq;
+    for(int num:nums){
+        freq[num]++;
+    }
+    int maxfreq=0;
+    for(auto& [num,count]:freq)
+    maxfreq=max(maxfreq,count);
 
+    //total no of elements with that freq
+    int total=0;
+    for(auto& [num,count]:freq)
+    if(count==maxfreq)
+    total+=count;
+
+    return total;
+
+    }
+};
    
