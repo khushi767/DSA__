53df41be63abe0f8d7cb857f594904745d074b16
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-10-28 15:13:15 +0530

    Time: 3 ms (2.90%) | Memory: 19.1 MB (45.02%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -12,9 +12,31 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
+/*
+        Brute Force (Recursive DFS — very intuitive)
+        TC=O(N), SC=O(H)height of tree
         if(root==NULL)return 0;
         int leftDepth=maxDepth(root->left);
         int rightDepth=maxDepth(root->right);
         return 1+max(leftDepth,rightDepth);
+*/
+
+if(!root)return 0;
+queue<TreeNode*>q;
+q.push(root);
+int depth=0;
+while(!q.empty()){//As long as there are nodes left to process in the queue,
+int size=q.size();//tells how many nodes are at current level
+depth++;
+for(int i=0;i<size;i++){
+    TreeNode* node=q.front();q.pop();
+    if(node->left)q.push(node->left);
+    if(node->right)q.push(node->right);
+
+}
+
+}
+return depth;
+
     }
 };
