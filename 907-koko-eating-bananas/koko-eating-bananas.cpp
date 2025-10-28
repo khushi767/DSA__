789129c325fb3f4eb4fc553677fe767df8a8ea74
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-10-28 11:37:09 +0530

    Time: 4 ms (88.35%) | Memory: 22.9 MB (44.80%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,28 +1,8 @@
-/*class Solution {
+class Solution {
 public:
-bool canEatAll(vector<int>& piles,int h,int k){
-    long long hours=0;
-    for(int p:piles){
-        hours+=(p+k-1)/k;
-    }
-    return hours<=h;
-}
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int left=1;
-        int right=*max_element(piles.begin(),piles.end());
-        int ans=right;
-        while(left<=right){
-            int mid=left+(right/left)/2;
-            if(canEatAll(piles,h,mid)){
-                ans=mid;
-                right=mid-1;
-            }else{
-                left=mid+1;
-            }
-        }
-        return ans;
 
-        /*linear search
+    int minEatingSpeed(vector<int>& piles, int h) {
+/*linear search
         Time: O(max(piles) × n) — ❌ Too slow (max(piles) ≤ 1e9), Space: O(1)
         int maxPile=*max_element(piles.begin(),piles.end());
         for(int k=1;k<maxPile;k++){
@@ -33,31 +13,27 @@ bool canEatAll(vector<int>& piles,int h,int k){
             if(hours<=h)return k;
         }
         return maxPile;*/
+int left=1;int right=*max_element(piles.begin(),piles.end());
+int ans=right;
+while(left<=right){
+int mid=left+(right-left)/2;
+long hours=0;
+for(int p:piles){
+    hours+=(p+mid-1)/mid;
 
+}
+if(hours<=h){
+    ans=mid;
+    right=mid-1;
+}
+else{
+    left=mid+1;
+}
+}
+return ans;
 
-//    }
-//};
-
-class Solution {
-public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int l = 1, r = *max_element(piles.begin(), piles.end());
-        int ans = r;
-
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            long hours = 0;
-            for (int p : piles)
-                hours += (p + mid - 1) / mid;
-
-            if (hours <= h) {
-                ans = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-
-        return ans;
     }
 };
+
+
+
