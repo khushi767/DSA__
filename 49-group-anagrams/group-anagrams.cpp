d14d0d0ff752532d35d9d4ff1e3c221be47ad1fa
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-11-07 04:22:22 +0530

    Time: 20 ms (40.95%) | Memory: 24.8 MB (86.00%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,15 +1,49 @@
 class Solution {
 public:
+/*
+Brute force
+Sorting each comparison → O(k log k) where k = length of string
+Comparing each pair → O(n²)
+\U0001f449 Total: O(n² * k log k)
+This is very slow for n = 10⁴.
+bool isAnagram(string a,string b){
+    if(a.size()!=b.size())return false;
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+    return a==b;
+}
+*/
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        /*Brute force
+        vector<vector<string>>res;
+        vector<bool>visited(strs.size(),false);
+        for(int i=0;i<strs.size();i++){
+            if(visited[i])continue;
+            vector<string>group={strs[i]};
+            visited[i]=true;
+
+            for(int j=i+1;j<strs.size();j++){
+                if(!visited[j]&& isAnagram(strs[i],strs[j])){
+                    group.push_back(strs[j]);
+                    visited[j]=true;
+                }
+            }
+            res.push_back(group);
+        }
+        return res;*/
+
         unordered_map<string,vector<string>>mp;
         for(string s:strs){
             string key=s;
             sort(key.begin(),key.end());
             mp[key].push_back(s);
         }
-        vector<vector<string>>ans;
-        for(auto& it:mp)
-        ans.push_back(it.second);
-        return ans;
+        vector<vector<string>>res;
+        for(auto &p:mp)
+        res.push_back(p.second);
+        return res;
+
+ 
     }
 };
