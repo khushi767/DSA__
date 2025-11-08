3292a7030d625723e8abba021469b31fbb35bc97
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-11-09 03:49:19 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (78.58%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -3,6 +3,16 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int n=nums.size();
         if(n==0)return 0;
+        int i=1;
+        for(int j=1;j<n;j++){
+            if(nums[j]!=nums[j-1]){
+                nums[i]=nums[j];
+                ++i;
+            }
+        }
+        return i;
+     /*   int n=nums.size();
+        if(n==0)return 0;
         vector<int>unique;
         unique.reserve(n);
         unique.push_back(nums[0]);
@@ -14,7 +24,7 @@ public:
         for(int i=0;i<(int)unique.size();i++){
             nums[i]=unique[i];
         }
-        return unique.size();
+        return unique.size();*/
         /*
         Brute-force (in-place, O(n²) time, O(1) extra space
         int n=nums.size();
