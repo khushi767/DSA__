07ed4f0f55fac544b850ae0a8e1c147a2116d4d7
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-11-11 01:24:22 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (32.32%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        /*
+       // better approach: Time: O(n) average (for the algorithm it performs), SC=O(1)
+        next_permutation(nums.begin(),nums.end());*/
+
+        int n=nums.size();
+        int pivot=n-2;
+        while(pivot>=0 && nums[pivot]>=nums[pivot+1]){
+            --pivot;
+        }
+        if(pivot<0){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        int successor=n-1;
+        while(successor>pivot && nums[successor]<=nums[pivot]){
+            --successor;
+        }
+        swap(nums[pivot],nums[successor]);
+        reverse(nums.begin()+pivot+1,nums.end());
+
+
     }
 };
