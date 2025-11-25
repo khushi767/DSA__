efcf8df74104e1a0c205f8c6cb6338f55744e624
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-11-26 01:04:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (15.75%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         /*
-        for(int i=0;i<=30;i++){
+        for(int i=0;i<=30;i++){ //loop runs for 31 times, TC=O(1), SC=O(1)
             int ans=pow(2,i);
             if(ans==n){
                 return true;
             }
         }
         return false;*/
-
+/*
         int ans=1;
-        for(int i=0;i<=30;i++){
+        for(int i=0;i<=30;i++){ TC=O(1),SC=O(1)
             if(ans==n){
                 return true;
             }
             if(ans<INT_MAX/2)
              ans=ans*2;
         }
-        return false;
+        return false;*/
+return n > 0 && (n & (n - 1)) == 0;
+
+
     }
 };
