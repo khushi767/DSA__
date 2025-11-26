7dcf9b82ce6f4ade7a206de1a6ac3d1ffe17c100
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-11-26 12:34:35 +0530

    Time: 12 ms (12.48%) | Memory: 23.8 MB (12.13%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        
+        /* Brute force TC=O(n2),SC=O(1)
         for(int i=0;i<nums.size();i++){
             int count=0;
             for(int j=0;j<nums.size();j++){
@@ -11,12 +11,29 @@ public:
             if(count==1)return nums[i];
         }
         return -1;
-        /*
+        */
+        /* optimal using xor
         int ans=0;
         for(int i=0;i<nums.size();i++){
             ans=ans^nums[i];
         }
         return ans;
         */
+
+/*better using sorting: Time O(n log n) (due to sort), Space O(1)*/
+/*sort(nums.begin(),nums.end());
+int n=nums.size();
+for(int i=0;i<n-1;i+=2){
+    if(nums[i]!=nums[i+1])return nums[i];
+}
+return nums.back();*/
+
+/*using hashmap: Time O(n), Space O(n).*/
+unordered_map<int,int>cnt;
+for(int x:nums)cnt[x]++;
+for(auto &p:cnt)if(p.second==1)return p.first;
+return -1;
     }
 };
+
+
