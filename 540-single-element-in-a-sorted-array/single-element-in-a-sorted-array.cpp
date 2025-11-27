c1abb741f77206b14282943ff09241cc5adc6609
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-11-28 01:36:23 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (67.69%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        /* Brute approach: O(n)
         for(int i=0;i<nums.size()-1;i+=2){
             if(nums[i]!=nums[i+1]){
                 return nums[i];
             }
         }
         return nums.back();//last element is single
+        */
+
+        //better approach: using xor a^a=0, a^0=a, TC=O(n)
+        int x=0;
+        for(int n:nums)
+        x=x^n;
+        return x;
     }
 };
 
