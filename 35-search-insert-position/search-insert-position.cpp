d8b7f67678878131b8033c09d0269900d16f758d
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-11-28 01:15:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (5.62%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -9,19 +9,17 @@ public:
        return nums.size();//If loop finishes fully, it means:target is greater than ALL elements in the array.So target should be inserted at the end.
        */
        //using stl: TC=O(logn)
-       return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+    //   return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+
+   // optimal approach: using binary search TC=O(logn)
+
+   int low=0,high=nums.size()-1;
+   while(low<=high){
+    int mid=low+(high-low)/2;
+    if(nums[mid]==target)return mid;
+    else if(nums[mid]<target)low=mid+1;
+    else high=mid-1;
+   }
+   return low;
     }
 };
-
-/*
-int searchInsert(vector<int>& nums, int target) {
-    for(int i = 0; i < nums.size(); i++){
-        if(nums[i] >= target) return i;
-    }
-    return nums.size();
-}
-
-int searchInsert(vector<int>& nums, int target) {
-    return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-}
-*/
