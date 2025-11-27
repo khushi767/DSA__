1dff74665589574f5b6890986470d693ee5e690a
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-11-28 01:11:54 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (5.62%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
+        /*
+        Brute approach: TC= O(n)
        for(int i=0;i<nums.size();i++){
         if(nums[i]>=target)return i;
        } 
-       return nums.size();
+       return nums.size();//If loop finishes fully, it means:target is greater than ALL elements in the array.So target should be inserted at the end.
+       */
+       //using stl: TC=O(logn)
+       return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
     }
 };
 
