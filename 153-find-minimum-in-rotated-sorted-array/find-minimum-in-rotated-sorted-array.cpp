85e67f0a7ec8581e1af68d615a298a630fb8bf6c
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-12-07 03:40:34 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (12.67%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -7,10 +7,25 @@ public:
         return mn;
 
         */
+        /*Better, TC=O(n) in worst-case (if array is not rotated or drop is at end).Space complexity: O(1).
         int n=nums.size();
         for(int i=0;i<n-1;i++){
             if(nums[i]>nums[i+1])return nums[i+1];
         }
         return nums[0];
+*/
+        //optimal : using binary approach
+        int low=0,high=nums.size()-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>nums[high]){
+                // min is in right half
+                low=mid+1;
+            }else{
+                // nums[mid] <= nums[high], min is at mid or left
+                high=mid;
+            }
+        }
+        return nums[low];
     }
 };
